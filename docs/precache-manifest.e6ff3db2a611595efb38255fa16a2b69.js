9f63ee07dd20741f7f41c6398f02e01e23f81b7a
agusmafud <agusmafud@gmail.com>
2019-03-07 12:34:44 -0300

    Build Github Pages

---
+++ docs/precache-manifest.e6ff3db2a611595efb38255fa16a2b69.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddbe80012152e5ae4fc49ec7be6190ec",
+    "url": "/static/media/sophenia.ddbe8001.jpg"
+  },
+  {
+    "revision": "b0cde7cb0b992487dd2cad701df7c7a2",
+    "url": "/static/media/marcus.b0cde7cb.jpg"
+  },
+  {
+    "revision": "779c1dddac0ff99bea6340f7fe16259d",
+    "url": "/static/media/m2.779c1ddd.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5a1abea3f8a1f8fb5761",
+    "url": "/static/js/main.5a1abea3.chunk.js"
+  },
+  {
+    "revision": "4523ce5134db732b8e46",
+    "url": "/static/js/2.4523ce51.chunk.js"
+  },
+  {
+    "revision": "4523ce5134db732b8e46",
+    "url": "/static/css/2.ea90e6b9.chunk.css"
+  },
+  {
+    "revision": "abcb193008cc77c6f6b18fafd96fcc52",
+    "url": "/index.html"
+  }
+];
