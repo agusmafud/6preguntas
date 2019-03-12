d20624ddca551228756afe9bdf105b2bb9b4ce64
agusmafud <agusmafud@gmail.com>
2019-03-12 17:16:17 -0300

    Updates

---
+++ precache-manifest.321a4de178bd48456a977fac0df277cc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddbe80012152e5ae4fc49ec7be6190ec",
+    "url": "/6preguntas/static/media/sophenia.ddbe8001.jpg"
+  },
+  {
+    "revision": "b0cde7cb0b992487dd2cad701df7c7a2",
+    "url": "/6preguntas/static/media/marcus.b0cde7cb.jpg"
+  },
+  {
+    "revision": "779c1dddac0ff99bea6340f7fe16259d",
+    "url": "/6preguntas/static/media/m2.779c1ddd.jpg"
+  },
+  {
+    "revision": "dbe1720683c68641e53e",
+    "url": "/6preguntas/static/js/runtime~main.dbe17206.js"
+  },
+  {
+    "revision": "439f6b26a5a7c0d0c50a",
+    "url": "/6preguntas/static/js/main.439f6b26.chunk.js"
+  },
+  {
+    "revision": "4523ce5134db732b8e46",
+    "url": "/6preguntas/static/js/2.4523ce51.chunk.js"
+  },
+  {
+    "revision": "4523ce5134db732b8e46",
+    "url": "/6preguntas/static/css/2.ea90e6b9.chunk.css"
+  },
+  {
+    "revision": "66c98781125524c1059c08d41c03bbdd",
+    "url": "/6preguntas/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8cecf69a1b42f6517682e713632fc6e1.js"
+  "/6preguntas/precache-manifest.321a4de178bd48456a977fac0df277cc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/6preguntas/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
